a499fbf4d023d8a479eb31f7bfc542cd5713fc11
Pranali-alt <farkadepranali3@gmail.com>
2025-03-30 01:02:52 +0530

    arraylist code added for city

---
+++ CollectionDemo/src/com/test/jan/ArraylistDemo.java
@@ -0,0 +1,18 @@
+package com.test.jan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArraylistDemo {
+	public static void main(String[] args) {
+		
+	
+	List<String> list = new ArrayList<>();
+	list.add("pune");
+	list.add("mumbai");
+	list.add("banglore");
+	list.add("delhi");
+	System.out.println(list);
+	
+	}
+}
